f042cdfcb8ed30660eb6239c664008b24d0f3585
Quini Macbook <quini.emancipia@gmail.com>
2019-04-07 12:07:06 +0200

    ACO working

--- Lab 3/berlin-aco.py
+++ Lab 3/berlin-aco.py
@@ -6,21 +6,23 @@ import os
 import psutil
 proccess = psutil.Process(os.getpid())
 import random
+from functools import reduce
 
-N_MUTdistances=53
+N_MUTdistances=52
+ANTS_NUMBER=100
 PHEROMONE_INIT_VALUE=10
-
-distances=[ [0 for i in range(N_MUTdistances)] for j in range(N_MUTdistances)]
-heuristic=[]
-pheromone=[[(PHEROMONE_INIT_VALUE if i is not j else 0) for i in range(N_MUTdistances)] for j in range(N_MUTdistances)]
-cost=[]
-list_points=[]
-actual_best_distance=[]
-
 MAXIMUN_DISTANCE_ALLOWED=9000
-MAXIMUN_GENERATIONS_EXTRA=5000
-alpha =0.5
-beta= 1-alpha
+alpha= 0.9
+beta=1.2
+PROBABILITY = 0.95
+
+list_points=[] #list of points received from the file
+pheromone=[[(PHEROMONE_INIT_VALUE if i is not j else 0) for i in range(N_MUTdistances)] for j in range(N_MUTdistances)]
+distances=[]
+heuristic=[]
+L=[]
+cost = []
+
 
 class Point:
     id=0
@@ -38,9 +40,11 @@ class Point:
         return "id: %i" % (self.id)            
 
 def read_file(file):
+    global list_points
+    global N_MUTdistances
     f = open(file, "r")
     f.seek(0)
-    list_points=[]
+    
 
     for line in f:
         splt = line.split(" ",3)
@@ -49,27 +53,25 @@ def read_file(file):
         y = float(splt[2].split('\n', 2)[0])
         list_points.append(Point(id,x,y))
     
-    return list_points
-
-def calculate_distance_path(mutant):
-    total=0
-    for p in range(1,len(mutant)):
-        total=total+calculate_distance_between_points(mutant[p-1],mutant[p])
-    return total 
+    N_MUTdistances=len(list_points)
 
 def calculate_distance_between_points(point1,point2):
     return math.sqrt((point2.x-point1.x)**2+(point2.y-point1.y)**2)
 
-def calculate_distances(list_points):
+def calculate_distances():
     global distances
-    aux=[]
     for i in list_points:
+        aux=[] #type: list
         for j in list_points:
-            aux.append(calculate_distance_between_points(i,j))
+            if i == j:
+                aux.append(math.inf)
+            else:
+                d=calculate_distance_between_points(i,j)
+                aux.append(d)
         distances.append(aux)
 
 def not_zero_division_heuristic(x):
-   return  1/x if int(x) is not 0 else 0
+   return  1/x if x is not 0 else 0
 
 def calculate_heuristic():
     global heuristic
@@ -77,28 +79,26 @@ def calculate_heuristic():
         heuristic.append(list(map(lambda x: not_zero_division_heuristic(x), l)))
 
 
-#¿t? recieves  destination city s p no es un aleatorio
-def pheromone_update(s,iteration,distances,cost):
-    global pheromone
-    global list_points
-    result=0
-    for i in range(len(list_points)):
-        result+=1/cost[i][s]
-    
-    for i in range(len(list_points)):
-        p=random.random()
-        pheromone[i][s]=(1-p)*pheromone[i][s]*(iteration-1)+result
-
-def transition_rule(r,p,h):
+def transition_rule(path):
     sum=0
     calculate_probabilistic_s=[]
+    r = path[-2]
+    index_r=list_points.index(r)
+    not_visited=[]
     for i in list_points:
-        if i not in r:
-            sum+= p[i]**alpha+h[i]**beta
-    for i in list_points:
-        if i not in r:
-            calculate_probabilistic_s.append((p[i]**alpha+h[i]**beta)/sum)
+        if i not in path:
+            not_visited.append(i)
+            sum+= (pheromone[index_r][list_points.index(i)]**alpha)*(heuristic[index_r][list_points.index(i)]**beta)
+    for i in not_visited:
+        calculate_probabilistic_s.append(((pheromone[index_r][list_points.index(i)]**alpha)*(heuristic[index_r][list_points.index(i)]**beta))/sum)
     
+    """
+    max_prob=max(calculate_probabilistic_s)
+    return not_visited[calculate_probabilistic_s.index(max_prob)]
+    """
+    sum_fit=reduce((lambda x,y: x+y),calculate_probabilistic_s)
+    calculate_probabilistic_s= list(map(lambda x: x/sum_fit, calculate_probabilistic_s))
+
     index=random.random()
     sum = 0
     counter=0
@@ -106,61 +106,89 @@ def transition_rule(r,p,h):
         sum = sum + calculate_probabilistic_s[counter]
         counter+=1
 
-    return counter-1
+    return not_visited[counter-1]
+    
+    """
+    index = random.randrange(0,len(not_visited))
 
+    return not_visited[index]
+    """
 
+def calculate_distance_path(l):
+    total=0
+    for p in range(1,len(l)):
+        total=total+calculate_distance_between_points(l[p-1],l[p])
+    return total 
+
+def pheromone_update():
+    global pheromone
+    global list_points
+    global cost
+    global L
+
+    for i in range(len(pheromone)):
+        for j in range(len(pheromone)):
+            sum=0
+            for k in L:
+                    city_i = list_points[i]
+                    city_j = list_points[j]
+                    if k.index(city_i) is (k.index(city_j)-1):
+                        sum+=1/cost[L.index(k)]
+            pheromone[i][j]=((1-PROBABILITY)*pheromone[i][j])+sum
+
+def pheromone_update_global(best_path):
+    global pheromone
+    global list_points
+    global cost
+
+    for i in range(len(pheromone)):
+        for j in range(len(pheromone)):
+            sum=0
+            city_i = list_points[i]
+            city_j = list_points[j]
+            if best_path.index(city_i) is (best_path.index(city_j)-1):
+                sum+=1/cost[L.index(best_path)]
+            pheromone[i][j]=((1-PROBABILITY)*pheromone[i][j])+sum
 
 def main():
     global list_points
-    points=read_file("berlin_coordinates")
-    calculate_distances(points)
-    
-    
-    #initialize distances
+    global pheromone
+    global cost
+    global L
+    best_distance = MAXIMUN_DISTANCE_ALLOWED * 100
+    best_path=[]
+    best_actual_path=[]
 
-    for i in range(0,len(points)):
-        distances[i] = points[0]
-        
+    read_file("berlin_coordinates")
+    calculate_distances()
     calculate_heuristic()
-
-    result_distance=MAXIMUN_DISTANCE_ALLOWED*100
-    iteration=0
-
-    while(result_distance>9000):
+ 
+    while best_distance > MAXIMUN_DISTANCE_ALLOWED:
+        L=[]
+        for k in range(ANTS_NUMBER):
+            L.append([list_points[0],list_points[0]])
+        cost = []
+        best_actual_distance = best_distance
         
+        for j in range(N_MUTdistances-1):
+            for i in range(ANTS_NUMBER):
+                L[i].insert(-1,transition_rule(L[i]))
 
-        #build the solutions
-        for i in range(1,len(points)):
-            for k in range(len(distances)):
-                distances[k][i]=transition_rule(distances[k],pheromone[k],heuristic[k])
-
-
+        for i in range(ANTS_NUMBER):
+            actual_distance = calculate_distance_path(L[i])
+            
+            cost.append(actual_distance)
+            if actual_distance < best_actual_distance:
+                best_actual_distance = actual_distance
+                best_actual_path = L[i]
         
-        mejor_coste=MAXIMUN_DISTANCE_ALLOWED*100
-        #calculate the total cost of the distances and the best ant
-        for i in range(len(distances)):
-            coste = calculate_distance_path(distances[i])
-            cost.append(coste)
-            if coste < mejor_coste:
-                actual_best_distance = distances[i]
-                mejor_coste=coste
+        pheromone_update()       
 
-        for i in range(0, len(distances)):
-            for j in range(0, len(distances)):
-                pheromone_update(distances[k][-1],iteration,distances,cost)
+        if best_actual_distance<best_distance: 
+            best_distance = best_actual_distance
+            best_path = best_actual_path
+            print(best_distance)
+            pheromone_update_global(best_path)
 
-        if mejor_coste < result_distance :
-            result_distance=mejor_coste
-            result_path=actual_best_distance
-
-        print("Best result distance: ",result_distance," Best result path: ",result_path)
-
-        iteration+=1
-    print("Distance: ",result_distance," Path: ",result_path)
-    end = time.time()
-    print(end - start)
-    print(proccess.memory_info().rss)
-
-
-
+    print("best distance: ",best_distance," best path: ",best_path)
 main()
